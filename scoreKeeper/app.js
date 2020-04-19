ebc1a31accf0c3e3508a8243d0947d9b6fd9de84
AbdulMalik <abdumaliksharipov@gmail.com>
2020-04-19 03:00:12 -0500

    Reset functionality

--- scoreKeeper/app.js
+++ scoreKeeper/app.js
@@ -13,10 +13,13 @@ let score2=0;
 let gameOver=false;
 let winningScore=5;
 
+let resetBtn=document.getElementById("reset");
+
 p1Button.addEventListener("click",function(){
     if(!gameOver){
         score1++;
         if(score1===winningScore){
+            valueOne.classList.add("changeCol");
             gameOver=true;
         }        
         valueOne.textContent=score1;
@@ -28,8 +31,20 @@ p2Button.addEventListener("click",function(){
     if(!gameOver){
         score2++;
         if(score2===winningScore){
+            valueTwo.classList.add("changeCol");
             gameOver=true;
         }        
         valueTwo.textContent=score2;
     }    
 })
+
+resetBtn.addEventListener("click",resetPage);
+
+function resetPage(){
+    score1=0;
+    score2=0;
+    valueOne.textContent=0;
+    valueTwo.textContent=0;
+    valueOne.classList.remove("changeCol");
+    valueTwo.classList.remove("changeCol");
+}
