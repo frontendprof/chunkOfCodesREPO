7eeef3a5d2b8a0d9d47fc2f50c56bec6e379c630
AbdulMalik <abdumaliksharipov@gmail.com>
2020-04-20 00:10:29 -0500

    Finalizing list item events

--- listItemEvents/app.js
+++ listItemEvents/app.js
@@ -14,3 +14,10 @@ for(let i=0;i<lis.length;i++){
         this.classList.remove("colored");
     })
 }
+
+
+for(let i=0;i<lis.length;i++){
+    lis[i].addEventListener('click',function(){
+        this.classList.toggle("colorless");
+    })
+}
